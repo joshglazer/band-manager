6a10b72d47561308affa6cb09c7383b86bba5f03
Josh Glazer <joshglazer@gmail.com>
2023-12-30 22:58:01 -0500

    feat(mui): integrate mui and implement basic designs for all pages

--- components/layout/AuthButton.tsx
+++ components/layout/AuthButton.tsx
@@ -2,6 +2,7 @@ import { createClient } from '@/utils/supabase/server';
 import Link from 'next/link';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import Button from '@mui/material/Button';
 
 export default async function AuthButton() {
   const cookieStore = cookies();
@@ -24,9 +25,12 @@ export default async function AuthButton() {
     <div className="flex items-center gap-4 text-sm">
       Hey, {user.email}!
       <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <Button
+          variant="outlined"
+          className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+        >
           Logout
-        </button>
+        </Button>
       </form>
     </div>
   ) : (
